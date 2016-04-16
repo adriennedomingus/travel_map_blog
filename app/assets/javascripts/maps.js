903140992f275969631d5607d554bb654f5e0f53
Adrienne Domingus <adrienne.domingus@gmail.com>
2016-04-16 12:31:12 -0600

    Google map shows up on front page

---
+++ app/assets/javascripts/maps.js
@@ -0,0 +1,11 @@
+$( document ).ready(function() {
+  initMap();
+});
+
+var map;
+function initMap() {
+  map = new google.maps.Map(document.getElementById('map'), {
+    center: {lat: 50.0, lng: 0},
+    zoom: 2
+  });
+}

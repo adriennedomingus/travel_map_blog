04de914458d1ae489dbf34248d8746438c9fa579
Adrienne Domingus <adrienne.domingus@gmail.com>
2016-04-16 14:10:33 -0600

    correct test index

--- spec/features/user_views_list_of_blogs_for_specific_user_spec.rb
+++ spec/features/user_views_list_of_blogs_for_specific_user_spec.rb
@@ -3,9 +3,9 @@ require 'rails_helper'
 RSpec.feature "user views index for specific user" do
   scenario "user  views index for specific user" do
     user = User.create(provider: "twitter", uid: "1234", nickname: "adomingus", token: ENV['USER_TOKEN'], secret:  ENV['USER_SECRET'])
-    b1 = user.blogs.create(title: "title", date: "2016/03/19", content: "some content")
-    b2 = user.blogs.create(title: "other title", date: "2016/03/25", content: "some more content")
-    b3 = user.blogs.create(title: "third title", date: "2016/03/29", content: "final content")
+    b1 = user.blogs.create(title: "title", date: "2016/03/19", content: "some content", slug: "title")
+    b2 = user.blogs.create(title: "other title", date: "2016/03/25", content: "some more content", slug: "other-title")
+    b3 = user.blogs.create(title: "third title", date: "2016/03/29", content: "final content", slug: "third-title")
 
     visit users_blogs_path(user)
     expect(page).to have_content(b1.title)

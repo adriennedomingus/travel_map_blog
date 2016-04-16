7f5b329e916497492d22a9c089d4c7c8b1b4df6b
Adrienne Domingus <adrienne.domingus@gmail.com>
2016-04-16 14:36:46 -0600

    debugging

--- app/controllers/user/blogs_controller.rb
+++ app/controllers/user/blogs_controller.rb
@@ -9,9 +9,9 @@ class User::BlogsController < User::BaseController
     @blog = Blog.new(blog_params)
     if @blog.save
       current_user.blogs << @blog
-      @blog.update_attribute(:slug, @blog.title.parameterize)
       require "pry"
       binding.pry
+      @blog.update_attribute(:slug, @blog.title.parameterize)
       service = TwitterService.new(current_user)
       service.post_tweet(blog_url(@blog.slug))
       redirect_to blog_path(@blog.slug)

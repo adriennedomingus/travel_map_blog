a8972d1e4ceba4f6e60fc6c87c66edfef6a00527
Adrienne Domingus <adrienne.domingus@gmail.com>
2016-04-20 17:46:11 -0600

    get image from blog based on weather

--- app/models/blog.rb
+++ app/models/blog.rb
@@ -2,7 +2,7 @@ class Blog < ActiveRecord::Base
   belongs_to :user
   belongs_to :trip
   has_many :photos
-  
+
   before_save :set_slug
   before_save :set_color
   before_save :set_weather
@@ -34,5 +34,10 @@ class Blog < ActiveRecord::Base
 
   def set_weather
     self.weather = Weather.description(self)
+  end
+
+  def get_image
+    term = self.weather.split(" ")[-1]
+    UnsplashService.new.random_by_search(term)
   end
 end
--- spec/models/blog_spec.rb
+++ spec/models/blog_spec.rb
@@ -6,4 +6,14 @@ RSpec.describe Blog, type: :model do
   it { should validate_presence_of :title }
   it { should validate_presence_of :date }
   it { should validate_presence_of :content }
+
+  it "returns an image based on weather" do
+    VCR.use_cassette("blog#get_image") do
+      user = create_user
+      _, b1 = create_trip_and_blog(user)
+
+      image = b1.get_image
+      expect(image.class).to eq(String)
+    end
+  end
 end

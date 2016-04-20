3e709f8af0263955575ac08f5d70ad1219fb1638
Adrienne Domingus <adrienne.domingus@gmail.com>
2016-04-20 09:37:10 -0600

    Starting views

--- app/controllers/user/photos_controller.rb
+++ app/controllers/user/photos_controller.rb
@@ -1,6 +1,6 @@
 class User::PhotosController < User::BaseController
   def index
     user = User.find_by(nickname: params[:nickname])
-    @photos = user.photos
+    @photos = user.photos.order(image_updated_at: :desc)
   end
 end

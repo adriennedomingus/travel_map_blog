593a4e619016bf8cb2f8f77e2807cb41f4acbcf5
Adrienne Domingus <adrienne.domingus@gmail.com>
2016-04-25 14:09:28 -0600

    Add user to blog show
    When visitor visit's other user's blog can now link back to their map via name on post

--- spec/features/user_views_individual_blog_spec.rb
+++ spec/features/user_views_individual_blog_spec.rb
@@ -11,6 +11,7 @@ RSpec.feature "user views specific blog" do
       expect(page).to have_content(b1.title)
       expect(page).to have_content(b1.posted_on)
       expect(page).to have_content(b1.formatted_date)
+      expect(page).to have_content(user.nickname)
       expect(page).to have_css("img[alt='Img 2013']")
     end
   end

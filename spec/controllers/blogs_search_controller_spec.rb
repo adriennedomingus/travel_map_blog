693214a5bf7a41911e0ca7641708f53aa1982540
Adrienne Domingus <adrienne.domingus@gmail.com>
2016-04-25 14:23:55 -0600

    Add controller test for blog search
    Multiple blogs, only returns those relevant to search

---
+++ spec/controllers/blogs_search_controller_spec.rb
@@ -0,0 +1,18 @@
+require 'rails_helper'
+
+RSpec.describe Blogs::SearchController, type: :controller do
+  describe "POST search" do
+    it "returns all blogs matching search criteria" do
+      VCR.use_cassette("blog.search") do
+        user = create_user
+        _, b1 = create_trip_and_blog(user)
+        create_second_trip_and_blog(user)
+        get :index, location: b1.location, radius: 50
+        blogs = JSON.parse(response.body)
+
+        expect(blogs.count).to eq(1)
+        expect(blogs.first["title"]).to eq(b1.title)
+      end
+    end
+  end
+end
--- spec/features/user_edits_a_photo_spec.rb
+++ spec/features/user_edits_a_photo_spec.rb
@@ -19,15 +19,16 @@ RSpec.feature "user edits a photo" do
   end
 
   scenario "user must include all information" do
-    VCR.use_cassette("blog.new") do
+    VCR.use_cassette("photo.edit") do
       user = create_user
-      create_trip_and_blog(user)
+      trip, blog = create_trip_and_blog(user)
       allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
+      photo = create_photo(blog, user, trip)
 
-      visit new_photo_path
+      visit edit_photo_path(photo)
       fill_in :photo_title, with: ""
       fill_in :photo_description, with: "Photo description"
-      click_on "Create Photo"
+      click_on "Update Photo"
 
       expect(page).to have_content("Please enter all information")
     end
